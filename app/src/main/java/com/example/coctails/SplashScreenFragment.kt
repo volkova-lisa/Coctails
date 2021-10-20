e9700a8f33a5eee8ab1f8323e89565f5fda87468
volkova-lisa <liza.volk999@gmail.com>
2021-10-20 23:57:05 +0300

    menu 1

--- app/src/main/java/com/example/coctails/SplashScreenFragment.kt
+++ app/src/main/java/com/example/coctails/SplashScreenFragment.kt
@@ -1,20 +1,13 @@
 package com.example.coctails
 
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.replace
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import com.example.coctails.databinding.ActivityMainBinding
 import com.example.coctails.databinding.FragmentSplashScreenBinding
+import com.example.coctails.main_screen.MainScreenFragment
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
--- app/src/main/java/com/example/coctails/main_screen/MainScreenFragment.kt
+++ app/src/main/java/com/example/coctails/main_screen/MainScreenFragment.kt
@@ -1,4 +1,4 @@
-package com.example.coctails
+package com.example.coctails.main_screen
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
