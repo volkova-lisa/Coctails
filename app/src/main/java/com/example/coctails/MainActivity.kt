af7c8f36dbcd439b7df65e602acccf8f9a560b6e
volkova-lisa <liza.volk999@gmail.com>
2021-10-21 00:36:27 +0300

    20 hours and this shit doesnt crash

--- app/src/main/java/com/example/coctails/MainActivity.kt
+++ app/src/main/java/com/example/coctails/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.coctails.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +22,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
 
-       // mNavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        //val navController = findNavController(NavHostFragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        mBinding.bottomNavigatinView.setupWithNavController(navController)
+
+
 
 //        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 //        val navController = navHostFragment.navController
